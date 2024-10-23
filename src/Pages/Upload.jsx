b485e648695db40cdfcbc927a4c2c8c82ac63721
wuraola babalola <babalolawuraola321@gmail.com>
2024-10-23 12:58:59 +0100

    upload pre-fix

--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 // import axios from 'axios';
 
 const UploadPage = () => {
-  // const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([]);
   const [message, setMessage] = useState('');
   const [showModal, setShowModal] = useState(false); // State to control modal visibility
 
